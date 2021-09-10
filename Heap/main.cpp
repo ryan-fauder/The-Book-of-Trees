6ae04ee9bdf865caa9d539a7fff8892241cc70e6
Ryan Auder <ryan.fauder@gmail.com>
2021-09-10 16:17:43 -0300

    f: tests were done

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -3,16 +3,8 @@
 #include "heap-queue.hpp"
 using namespace std;
 
-int main(){
-  VHeap<int> heap;
-  HeapMax<int> heap1;
-  HeapMin<int> heap2;
-  cout << heap.getsize();
-  cout << heap.get();
-  //cout << heap.pop();
-  
-  heap1.pop();
-  heap1.top();
+void testing_heap(VHeap<int> heap){
+  cout << heap.getsize() << endl;
 
   heap.push(15);
   heap.push(20);
@@ -23,5 +15,52 @@ int main(){
   cout << heap.getsize() << " ";
   cout << heap.get() << " ";
   cout << heap.pop() << " ";
+  cout << endl;
+}
+
+void testing_heap(HeapMax<int> heap){
+  cout << heap.size() << endl;
+
+  heap.push(15);
+  heap.push(20);
+  heap.push(25);
+  heap.push(10);
+
+  cout << heap.size() << " ";
+  cout << heap.top() << " ";
+  heap.pop();
+  cout << heap.top();
+  cout << endl;
+}
+
+void testing_heap(HeapMin<int> heap){
+  cout << heap.size() << endl;
+
+  heap.push(15);
+  heap.push(20);
+  heap.push(25);
+  heap.push(10);
+
+  cout << heap.size() << " ";
+  cout << heap.top() << " ";
+  heap.pop();
+  cout << heap.top();
+  cout << endl;
+}
+
+int main(){
+  VHeap<int> heap;
+  HeapMax<int> heap1;
+  HeapMin<int> heap2;
+
+  cout << "Testing Vector Heap" << endl;
+  testing_heap(heap);
+  cout << "----" << endl;
+  cout << "Testing Queue Max Heap" << endl;
+  testing_heap(heap1);
+  cout << "----" << endl;
+  cout << "Testing Queue Min Heap" << endl;
+  testing_heap(heap2);
+  cout << "----" << endl;
   return 0;
 }
